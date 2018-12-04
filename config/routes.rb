9d5f1a628fa8e7a2f728c420da6079e8700ba1bf
BrittanyTinnin <bntinnin7@gmail.com>
2018-12-04 13:05:04 -0500

    organized routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  root 'welcome#index'
+
+  get '/signup', to: 'users#new', as: 'signup'
+
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
 
 
   resources :quantities
