18f002446c6f3552db90acb9c38b87a2a9685fbd
BrittanyTinnin <bntinnin7@gmail.com>
2018-12-04 12:30:12 -0500

    updated models with associations

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,25 @@
 class RecipesController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def recipe_params
+  end
 end
