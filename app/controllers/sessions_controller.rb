fcecf4930f1f9988b7a156a622012c3b1f2b1806
BrittanyTinnin <bntinnin7@gmail.com>
2018-12-04 14:05:34 -0500

    updated sessions controller, login form, and controller helper methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = params[:username] = @user.id 
+      redirect_to recipe_path
+    else 
+      render :new
+    end
   end
 
   def destroy
     session.clear
     redirect_to login_path
   end
+
 end
