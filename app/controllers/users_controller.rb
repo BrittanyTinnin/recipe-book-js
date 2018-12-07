2c743f70e3c92ee337dff49eccebc2afb40ff77f
BrittanyTinnin <bntinnin7@gmail.com>
2018-12-07 18:34:57 -0500

    added validation error message to signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save!
+    if @user.save
       session[:user_id] = @user.id
       flash[:notice] = "You have successfully logged in."
       redirect_to recipes_path
