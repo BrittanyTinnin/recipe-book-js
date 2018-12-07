4b74680e3c3db1cc49d399bc554c47b7700b157a
BrittanyTinnin <bntinnin7@gmail.com>
2018-12-07 14:01:04 -0500

    deleted comments in route

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,4 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show] do
     resources :recipes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
