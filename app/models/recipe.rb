5343cb11c9e4aef1b2406fc243d7323e968cc292
BrittanyTinnin <bntinnin7@gmail.com>
2018-12-12 10:01:56 -0500

    test custom writer

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -13,6 +13,7 @@ class Recipe < ApplicationRecord
   end
 
   def quantities_attributes=(quantities_attributes)
+    binding.pry
     quantities_attributes.values.each do |quantity_attributes|
       unless quantity_attributes[:name].blank?
         quantity = Quantity.new(quantity_attributes)
