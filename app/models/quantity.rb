08fcfbd4efe2d6395a10d60b64c5b4d8d6ce5c59
BrittanyTinnin <bntinnin7@gmail.com>
2018-12-12 00:20:01 -0500

    update custom writer attribute

--- app/models/quantity.rb
+++ app/models/quantity.rb
@@ -2,14 +2,6 @@ class Quantity < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
 
-  # def filling_name
-  #   self.filling.filling_name if self.filling
-  # end
-
-  # def filling_name=(name)
-  #   self.filling = Filling.find_or_create_by(filling_name: name)
-  # end
-
 
   def name
     self.ingredient.name if self.ingredient
