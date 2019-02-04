6b2a6c8c1349ed6b45c0f9f65c32dc15e885ef2b
BrittanyTinnin <bntinnin7@gmail.com>
2019-02-04 11:14:34 -0500

    Add document ready function to recipe.js and test load function

---
+++ app/assets/javascripts/recipes.js
@@ -0,0 +1,6 @@
+//set up a document ready in order to detect when our HTML page has loaded, and the document is ready to be manipulated:
+$(document).ready(function() {
+  // list all event listener functions
+  console.log('recipe.js is loaded....')
+});
+
