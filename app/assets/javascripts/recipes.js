34faa1aecfd3ffd7c6348903d4218b4661dbe639
BrittanyTinnin <bntinnin7@gmail.com>
2019-02-04 20:56:16 -0500

    Add more JS functions to recipe.js

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   // list all event listener functions
   console.log('recipe.js is loaded....');
   listenAllRecipesClick();
+  listenMyRecipesClick();
 });
 
 
@@ -26,23 +27,40 @@ function getRecipes() {
     let recipes = myJson
     let recipeList = ""
     recipes.forEach((recipe) => {
-      recipeList += '<li>' + recipe.name + '</li>'
-    })
+      recipeList += '<li>' + '<a href="#">' + recipe.name + '</a>' + '</li>';
+    });
+    $('#recipe-list').html('<br>' + '<h3> All Recipes </h3>' + recipeList)
     // console.log(recipes);
-    
   });
 };
 
-function getAllSandwiches() {
-  $.get("/sandwiches.json", function(data){
-      let sandwiches = data
-      // console.log(sandwiches)
-      let emptystring = ""
-      sandwiches.forEach((sandwich) => {
-          emptystring += '<li>' + sandwich["sandwich_name"] + '</li>';
-      });
-      $("#get-sandwiches").html(emptystring)
+
+function listenMyRecipesClick() {
+  console.log('in my recipes click function')
+  let myRecipeEl = document.getElementById('my-recipes');
+  myRecipeEl.addEventListener('click', function(e){
+    e.preventDefault();
+    getMyRecipes();
   })
 }
 
+function getMyRecipes() {
+  console.log('in getMyRecipes click function')
+  
+}
 
+function getRecipes() {
+  fetch('/recipes.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    let recipes = myJson
+    let recipeList = ""
+    recipes.forEach((recipe) => {
+      recipeList += '<li>' + '<a href="#">' + recipe.name + '</a>' + '</li>';
+    });
+    $('#recipe-list').html('<br>' + '<h3> All Recipes </h3>' + recipeList)
+    // console.log(recipes);
+  });
+};
